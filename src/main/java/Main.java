3c216f7dbf42a30033cac5f4822c7ec5132cc534
Rohan Raj P <rohanraj134764@gmail.com>
2025-11-10 12:09:03 +0530

    adding REPL exit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class Main {
+	
+	private static Integer exitStatus=0;
+	
 	public static void main(String[] args) throws Exception {
 		// TODO: Uncomment the code below to pass the first stage
 		Scanner scanner = new Scanner(System.in);
@@ -8,7 +11,9 @@ public class Main {
 		while (true) {
 			System.out.print("$ ");
 			String command = scanner.nextLine();
-			if(command.equalsIgnoreCase("exit")) {
+			if(command.contains("exit")) {
+				String[] values = command.split(" ");
+				exitStatus = Integer.parseInt(values[1]);
 				break;
 			}
 			System.out.println(command + ": command not found");
