775d62880710d6948eb41d6a106401f029204d97
Rohan Raj P <rohanraj134764@gmail.com>
2025-11-10 11:57:00 +0530

    adding REPL loop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,17 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        // TODO: Uncomment the code below to pass the first stage
-         System.out.print("$ ");
-         
-         Scanner scanner = new Scanner(System.in);
-         String command = scanner.nextLine();
-         System.out.println(command+": command not found");
-    }
+	public static void main(String[] args) throws Exception {
+		// TODO: Uncomment the code below to pass the first stage
+		Scanner scanner = new Scanner(System.in);
+
+		while (true) {
+			System.out.print("$ ");
+			String command = scanner.nextLine();
+			if(command.equalsIgnoreCase("exit")) {
+				break;
+			}
+			System.out.println(command + ": command not found");
+		}
+	}
 }
